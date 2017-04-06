79140adfbab34c10b2b55408ded1f106f85cdaac
tianyu8969 <tianyu8969@qq.com>
2017-04-06 14:01:43 +0800

    Update amap.py

--- amap.py
+++ amap.py
@@ -20,8 +20,8 @@ json_name = 'data_amap.json'
 # 高德地图poi：http://lbs.amap.com/api/webservice/guide/api/search/#text
 # 请替换为自己申请的key值：申请Web服务API类型KEY http://lbs.amap.com/dev/
 url_amap = 'http://restapi.amap.com/v3/place/text?key=6159ef91602ee2dbd718fc7c30601397&keywords=卫生服务中心&types=090000&city=上海&citylimit=true&children=1&offset=20&page=pageindex&extensions=all'
-each_page_rec = 20  # 每页记录数据，强烈建议不超过25，若超过25可能造成访问报错
-which_pach = r'page=1'  # 显示页码
+page_size = 20  # 每页记录数据，强烈建议不超过25，若超过25可能造成访问报错
+page_index = r'page=1'  # 显示页码
 global total_record  # 定义全局变量，总行数
 # Excel表头
 hkeys = ['id', '行业类型', '医院名称', '医院类型', '医院地址', '联系电话', 'location', '省份代码', '省份名称', '城市代码', '城市名称', '区域代码', '区域名称',
@@ -54,10 +54,10 @@ def getPOIdata():
     global total_record
     print('获取POI数据开始')
     josn_data = get_data(1)
-    if (total_record % each_page_rec) != 0:
-        page_number = int(total_record / each_page_rec) + 2
+    if (total_record % page_size) != 0:
+        page_number = int(total_record / page_size) + 2
     else:
-        page_number = int(total_record / each_page_rec) + 1
+        page_number = int(total_record / page_size) + 1
 
     for each_page in range(2, page_number):
         josn_data.extend(get_data(each_page))
